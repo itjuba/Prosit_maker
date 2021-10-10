86f94f19ac2ac1c9c88e6250eeca817216e32572
Attig Nadjib <49757658+itjuba@users.noreply.github.com>
2021-10-11 00:36:49 +0100

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :prosit_maker, PrositMaker.Repo,
        username: "juba",
-       password: "Nadjib11051963",
+       password: "",
        database: "prosit_m",
        hostname: "localhost",
        show_sensitive_data_on_connection_error: true,
